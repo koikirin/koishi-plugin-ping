b6ec84a4db7cebf4008a42ad0a572d6a2b028458
Hieuzest <girkirin@hotmail.com>
2023-08-05 21:45:19 +0800

    fix: import for kon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Context, Dict, Schema, Loader, Time, Logger } from 'koishi'
+import { Context, Dict, Schema, Time, Logger } from 'koishi'
+import { Loader } from '@koishijs/loader'
 
 const logger = new Logger('ping')
 
@@ -13,7 +14,7 @@ export class Ping {
     if (!reg) return
     for (const key of Object.getOwnPropertyNames(reg)) {
       const i1 = key.indexOf('/'), i2 = key.indexOf(':')
-      const mkey = key.slice(0, i2 === i1 ? key.length: i2)
+      const mkey = key.slice(0, i2 === i1 ? key.length : i2)
       if (mkey === name) return [key, parent, reg[key]?.ctx]
       const res = this._findPlugin(name, reg[key]?.ctx)
       if (res) return res
@@ -39,7 +40,7 @@ export class Ping {
   }
 
   async reloadPlugin(plugin: string | Context) {
-    const [key, parent, _] = this.findPlugin(plugin)??[]
+    const [key, parent, _] = this.findPlugin(plugin) ?? []
     if (!key) return 'Not found'
     this.ctx.loader.unloadPlugin(parent, key)
     await this.ctx.loader.reloadPlugin(parent, key, parent.config[key])
@@ -69,7 +70,7 @@ export class Ping {
       this.botsRetry[session.sid] = 0
       return next()
     })
-    
+
     if (config.reloadAdapters.checkInterval) {
       const timer = setInterval(() => {
         if (this.checkCurfew()) return
@@ -77,21 +78,21 @@ export class Ping {
           if (config.reloadAdapters.intervals?.[bot.sid]
             && this.botsTime[bot.sid]
             && (Date.now() - this.botsTime[bot.sid] > config.reloadAdapters.intervals?.[bot.sid])) {
-              this.botsRetry[bot.sid] ++
-              ctx.logger('ping').info(`${bot.sid} not responding, check`)
-              if (this.botsRetry[bot.sid] > config.reloadAdapters.retries) {
-                ctx.logger('ping').info(`${bot.sid} not responding, reload`)
-                this.botsRetry[bot.sid] = 0
-                this.reloadPlugin(bot.ctx).catch(logger.error)
-              }
+            this.botsRetry[bot.sid]++
+            ctx.logger('ping').info(`${bot.sid} not responding, check`)
+            if (this.botsRetry[bot.sid] > config.reloadAdapters.retries) {
+              ctx.logger('ping').info(`${bot.sid} not responding, reload`)
+              this.botsRetry[bot.sid] = 0
+              this.reloadPlugin(bot.ctx).catch(logger.error)
+            }
           }
         })
       }, config.reloadAdapters.checkInterval)
       ctx.collect('reloadAdaptersCheck', () => (clearInterval(timer), true))
     }
-  
+
     ctx.command('ping', { authority: 3 }).action(() => 'pong')
-  
+
     ctx.on('bot-disconnect', async (client) => {
       if (client.sid != config.notifySid) {
         const bot = ctx.bots[config.notifySid]
@@ -99,16 +100,16 @@ export class Ping {
       }
     })
   }
-  
+
 }
 
 export namespace Ping {
-  
+
   type SingleNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-  type MarkerTime =`${SingleNumber| ''}${SingleNumber}:${SingleNumber}${SingleNumber}`
+  type MarkerTime = `${SingleNumber | ''}${SingleNumber}:${SingleNumber}${SingleNumber}`
 
   export function markerTimeToNumber(marker: MarkerTime) {
-    const [ hours, minutes ] = marker.split(':')
+    const [hours, minutes] = marker.split(':')
     return parseInt(hours) * 60 + parseInt(minutes)
   }
 
@@ -134,7 +135,7 @@ export namespace Ping {
     reloadAdapters: reloadAdaptersConfig
     curfew: MarkerTimeRange[]
   }
-  
+
   export const Config: Schema<Config> = Schema.object({
     notifySid: Schema.string(),
     notifyTarget: Schema.string(),
