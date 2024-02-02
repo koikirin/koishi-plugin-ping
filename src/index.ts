74d93b00bffd155e9babd804fdbc7bb8fca51ddd
Hieuzest <girkirin@hotmail.com>
2024-02-02 16:22:07 +0800

    feat: migrate to 4.16.7

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,9 @@
-import { Context, Dict, Logger, Schema, Time } from 'koishi'
-
-const logger = new Logger('ping')
-const kRecord = Symbol.for('koishi.loader.record')
+import { Context, Dict, Schema, Time } from 'koishi'
 
 export class Ping {
   botsTime: Dict<number>
   botsRetry: Dict<number>
   curfew: Ping.TimeRange[]
-
-  _findPlugin(name: string, parent: Context): [string, Context, Context] {
-    if (!parent) return
-    const reg = parent.scope[kRecord]
-    if (!reg) return
-    for (const key of Object.getOwnPropertyNames(reg)) {
-      const i1 = key.indexOf('/'), i2 = key.indexOf(':')
-      const mkey = key.slice(0, i2 === i1 ? key.length : i2)
-      if (mkey === name) return [key, parent, reg[key]?.ctx]
-      const res = this._findPlugin(name, reg[key]?.ctx)
-      if (res) return res
-    }
-  }
-
-  _findPluginC(plugin: Context, parent: Context): [string, Context, Context] {
-    if (!parent) return
-    const reg = parent.scope[kRecord]
-    if (!reg) return
-    for (const key of Object.getOwnPropertyNames(reg)) {
-      if (reg[key]?.ctx === plugin) return [key, parent, reg[key]?.ctx]
-      const res = this._findPluginC(plugin, reg[key]?.ctx)
-      if (res) return res
-    }
-  }
-
-  findPlugin(plugin: string | Context) {
-    if (typeof plugin === 'string') {
-      return this._findPlugin(plugin, this.ctx.loader.entry)
-    } else {
-      return this._findPluginC(plugin, this.ctx.loader.entry)
-    }
-  }
-
-  async reloadPlugin(plugin: string | Context) {
-    const [key, parent] = this.findPlugin(plugin) ?? []
-    if (!key) return 'Not found'
-    this.ctx.loader.unloadPlugin(parent, key)
-    await this.ctx.loader.reloadPlugin(parent, key, parent.config[key])
-  }
 
   checkCurfew() {
     const date = new Date()
@@ -77,14 +35,13 @@ export class Ping {
         if (this.checkCurfew()) return
         ctx.bots.forEach(bot => {
           if (config.reloadAdapters.intervals?.[bot.sid]
-            && this.botsTime[bot.sid]
-            && (Date.now() - this.botsTime[bot.sid] > config.reloadAdapters.intervals?.[bot.sid])) {
-            this.botsRetry[bot.sid]++
+            && (!this.botsTime[bot.sid] || Date.now() - this.botsTime[bot.sid] > config.reloadAdapters.intervals?.[bot.sid])) {
+            this.botsRetry[bot.sid] = (this.botsRetry[bot.sid] ?? 0) + 1
             ctx.logger('ping').info(`${bot.sid} not responding, check`)
             if (this.botsRetry[bot.sid] > config.reloadAdapters.retries) {
               ctx.logger('ping').info(`${bot.sid} not responding, reload`)
               this.botsRetry[bot.sid] = 0
-              this.reloadPlugin(bot.ctx).catch(logger.error)
+              bot.ctx.scope.update(bot.ctx.config, true)
             }
           }
         })
